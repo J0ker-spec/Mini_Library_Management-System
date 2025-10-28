793f5df9f290d374e0603c1a6e78cfdc70e5b851
Vidal <olujonesvidal@gmail.com>
2025-10-28 00:33:36 -1200

    Update operation.py

--- operation.py
+++ operation.py
@@ -3,7 +3,6 @@ books = {}
 members = []
 genres = ["Fiction", "Non-Fiction", "Sci-Fi"]
 
-# 1. Add a new book
 def add_book(isbn, title, author, genre, total_copies):
     if isbn not in books:
         if genre in genres:
@@ -14,7 +13,6 @@ def add_book(isbn, title, author, genre, total_copies):
     else:
         return "Book already exists!"
 
-# 2. Add a new member
 def add_member(id, name):
     existing_ids = [m["id"] for m in members]
     if id not in existing_ids:
@@ -23,15 +21,13 @@ def add_member(id, name):
     else:
         return "Member already exists!"
 
-# 3. Search a book by title or author
+
 def search_book(keyword):
     # Instead of loops, we’ll use a small trick with list comprehension
     result = next((b for b in books.values() if keyword in b["title"] or keyword in b["author"]), None)
     if result:
         return result
     return "Book not found!"
-
-# 4. Update book info
 def update_book(isbn, new_title, new_author, new_genre):
     if isbn in books:
         books[isbn]["title"] = new_title
@@ -40,14 +36,14 @@ def update_book(isbn, new_title, new_author, new_genre):
         return "Book updated!"
     return "Book not found!"
 
-# 5. Delete a book
+
 def delete_book(isbn):
     if isbn in books:
         del books[isbn]
         return "Book deleted!"
     return "Book not found!"
 
-# 6. Borrow book
+
 def borrow_book(member_id, isbn):
     member = next((m for m in members if m["id"] == member_id), None)
     if not member:
@@ -63,7 +59,7 @@ def borrow_book(member_id, isbn):
     books[isbn]["total_copies"] -= 1
     return "Book borrowed!"
 
-# 7. Return book
+
 def return_book(member_id, isbn):
     member = next((m for m in members if m["id"] == member_id), None)
     if not member:
@@ -74,3 +70,4 @@ def return_book(member_id, isbn):
     member["borrowed_books"].remove(isbn)
     books[isbn]["total_copies"] += 1
     return "Book returned!"
+
